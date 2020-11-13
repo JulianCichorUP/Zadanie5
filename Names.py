f26c3d1f17a7ab6b9c03851b2a6374292a8acfd7
JulianCichorUP <73111644+JulianCichorUP@users.noreply.github.com>
2020-11-13 21:32:04 +0100

    Add files via upload

---
+++ Names.py
@@ -0,0 +1,3 @@
+names=['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
+firstLetters=[name[0] for name in names]
+print(firstLetters)
---
+++ Randint.py
@@ -0,0 +1,3 @@
+import random
+numbers = [[random.randint(1,10) for i in range(4)] for j in range(5)]
+print(numbers)
